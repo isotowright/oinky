a9157a9585c2296ed7c4427008fbb3142a6845c8
Guillaume <guillaume@chabot.tv>
2015-01-24 19:41:46 +0000

    Add style v.1

---
+++ javascript/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+  var n=0;
+  setInterval(increment,1000);
+
+   var animationLength = 5000;
+    $('#savingbar').change(function(){ 
+    animationLength = $(this).val() * 1000;
+    animateLoad(animationLength);
+      });
+  
+  animateLoad(animationLength);
+
+  function increment(){
+    n++;
+    setCounter(n);
+  }
+
+  function setCounter(v){
+    var counter=$(".counter");
+    var old=counter.children(".counter-value");
+    var oldContent=old.children(".counter-value-mask");
+
+    var t=0.4;
+    var d=t*0.0;
+    var d2=t*0.3;
+    var padding=55;
+    var offset=5;
+    var w=old.data("w");
+    w+=padding;
+    TweenMax.to(old,t,{delay:d,x:w,ease:Quad.easeIn});
+    TweenMax.to(oldContent,t,{delay:d,x:-(w-offset),ease:Quad.easeIn});
+    setTimeout(function(){old.remove()},t*1000);
+    
+    var neu=$("<div/>").addClass("counter-value").appendTo(counter);
+    var neuContent=$("<div/>").addClass("counter-value-mask").appendTo(neu).text('£'+v);
+    var saving=$(".loading").css('height',v+'%');
+    
+    w=neuContent.width();
+    neu.data("w",w);
+    neu.css({
+      width:w
+    })
+    w+=padding;
+    TweenMax.from(neu,t,{delay:d2,x:-w});
+    TweenMax.from(neuContent,t,{delay:d2,x:w-offset});
+  }
+})
